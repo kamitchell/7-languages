82b7697873d4349987e9c1357c068daf8be32632
Kevin A. Mitchell <kam@datalogics.com>
2016-01-05 11:13:59 -0500

    primeFactors basically works for integers up to 9

--- haskell/PrimeFactors.hs
+++ haskell/PrimeFactors.hs
@@ -1,4 +1,10 @@
 module PrimeFactors (primeFactors) where
 
 primeFactors :: Integer -> [Integer]
-primeFactors x = []
+primeFactors 1 = []
+primeFactors x
+    | (x `mod` 2 == 0) && (x > 2) = 2 : (primeFactors (x `div` 2))
+    | (x `mod` 3 == 0) && (x > 3) = 3 : (primeFactors (x `div` 3))
+    | otherwise = [x]
+
+--  vim: set et sts=4 sw=4 ts=8 :

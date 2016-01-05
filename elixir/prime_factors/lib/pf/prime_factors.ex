13db5e78e29f036aa564aa06a2d89e0a572763f6
Kevin A. Mitchell <kam@datalogics.com>
2016-01-05 15:21:42 -0500

    Handle factorization of 2

--- elixir/prime_factors/lib/pf/prime_factors.ex
+++ elixir/prime_factors/lib/pf/prime_factors.ex
@@ -1,5 +1,9 @@
 defmodule PF.PrimeFactors do
   def prime_factors(num) do
-    []
+    if num == 2 do
+      [2]
+    else
+      []
+    end
   end
 end
--- elixir/prime_factors/test/pf/prime_factors_test.exs
+++ elixir/prime_factors/test/pf/prime_factors_test.exs
@@ -4,4 +4,8 @@ defmodule PF.PrimeFactorsTest do
   test "factors of 1 are []" do
     assert PF.PrimeFactors.prime_factors(1) == []
   end
+
+  test "factors of 2 are [2]" do
+    assert PF.PrimeFactors.prime_factors(2) == [2]
+  end
 end

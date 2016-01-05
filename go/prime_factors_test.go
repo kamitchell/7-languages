0c422bf5940cd2a0d470ecbbe4fdc02c33e051a2
Kevin A. Mitchell <kam@datalogics.com>
2016-01-05 16:18:21 -0500

    Go: Handle factorization up to 3

--- go/prime_factors_test.go
+++ go/prime_factors_test.go
@@ -9,11 +9,16 @@ import (
 
 var _ = Describe("Factors", func() {
 	It("has factors of 1 as []", func() {
-    Expect(prime_factors.Factors(1)).To(Equal([]int{}))
-  })
+		Expect(prime_factors.Factors(1)).To(Equal([]int{}))
+	})
 
-  It("has factors of 2 as [2]", func() {
-    Expect(prime_factors.Factors(2)).To(Equal([]int{2}))
-  })
+	It("has factors of 2 as [2]", func() {
+		Expect(prime_factors.Factors(2)).To(Equal([]int{2}))
+	})
+
+	It("has factors of 3 as [3]", func() {
+		Expect(prime_factors.Factors(3)).To(Equal([]int{3}))
+	})
 })
+
 //  vim: set et sts=2 sw=2 ts=2 :

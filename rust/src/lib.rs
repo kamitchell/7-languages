a72c7b89cb88696a644ff3c80045611fca8a5b8d
Kevin A. Mitchell <kam@datalogics.com>
2016-01-05 13:22:03 -0500

    Rust: Simple factors up to 4

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,8 +1,28 @@
+#[allow(dead_code)]
 fn prime_factors(num: i64) -> Vec<i64> {
-    vec![]
+    match num {
+        1 => vec![],
+        4 => vec![2,2],
+        _ => vec![num]
+    }
 }
 
 #[test]
 fn prime_factors_of_one() {
     assert_eq!(prime_factors(1), []);
+}
+
+#[test]
+fn prime_factors_of_two() {
+    assert_eq!(prime_factors(2), [2]);
+}
+
+#[test]
+fn prime_factors_of_three() {
+    assert_eq!(prime_factors(3), [3]);
+}
+
+#[test]
+fn prime_factors_of_four() {
+    assert_eq!(prime_factors(4), [2, 2]);
 }

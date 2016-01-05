6a1453cb71e5480df97cf51cbeec8eb2e209a2f4
Kevin A. Mitchell <kam@datalogics.com>
2016-01-05 15:14:21 -0500

    Scala: My solutions and tests.

--- scala/src/main/scala/PrimeFactors.scala
+++ scala/src/main/scala/PrimeFactors.scala
@@ -3,7 +3,18 @@ package com.crebma.primeFactors
 object PrimeFactors {
 
   def factors(num: Int) : List[Int] = {
-    List[Int]()
+    factors(num, 2)
   }
 
+  def factors(num: Int, candidate: Int) : List[Int] = {
+    if (num == 1) {
+      List[Int]()
+    } else if (num % candidate == 0) {
+      candidate :: factors(num / candidate, candidate)
+    } else {
+      factors(num, candidate + 1)
+    }
+  }
 }
+
+//  vim: set et sts=2 sw=2 ts=2 :

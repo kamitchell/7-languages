30da95b1cb4550a6e7760cdc9ef9713dcf8625e3
Kevin A. Mitchell <kam@datalogics.com>
2016-01-05 16:16:28 -0500

    Go: Factor 2

--- go/prime_factors_test.go
+++ go/prime_factors_test.go
@@ -11,4 +11,9 @@ var _ = Describe("Factors", func() {
 	It("has factors of 1 as []", func() {
     Expect(prime_factors.Factors(1)).To(Equal([]int{}))
   })
+
+  It("has factors of 2 as [2]", func() {
+    Expect(prime_factors.Factors(2)).To(Equal([]int{2}))
+  })
 })
+//  vim: set et sts=2 sw=2 ts=2 :
--- go/src/prime_factors/prime_factors.go
+++ go/src/prime_factors/prime_factors.go
@@ -1,5 +1,11 @@
 package prime_factors
 
 func Factors(num int) []int {
-  return []int{}
+  if num > 1 {
+    return []int{num}
+  } else {
+    return []int{}
+  }
 }
+
+//  vim: set et sts=2 sw=2 ts=2 :
